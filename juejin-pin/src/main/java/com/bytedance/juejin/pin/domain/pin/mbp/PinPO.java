b4718ee03df58641d70efa987a403d9386c589b7
tanghanzheng <tanghanzheng@landsky.com>
2022-11-03 11:02:13 +0800

    mbp

--- juejin-pin/src/main/java/com/bytedance/juejin/pin/domain/pin/mbp/PinPO.java
+++ juejin-pin/src/main/java/com/bytedance/juejin/pin/domain/pin/mbp/PinPO.java
@@ -1,12 +1,18 @@
 package com.bytedance.juejin.pin.domain.pin.mbp;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@TableName("t_pin")
 class PinPO {
 
+    @TableId(type = IdType.INPUT)
     private String id;
 
     private String content;
@@ -16,4 +22,7 @@ class PinPO {
     private String userId;
 
     private Date createTime;
+
+    @TableLogic
+    private Boolean deleted;
 }
