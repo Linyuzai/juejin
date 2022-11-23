6d81a7f4fce6022f8dd5a0b0d9a5030e91885e61
tanghanzheng <tanghanzheng@landsky.com>
2022-11-23 15:52:40 +0800

    swagger user

--- juejin-user/src/main/java/com/bytedance/juejin/user/domain/user/UserController.java
+++ juejin-user/src/main/java/com/bytedance/juejin/user/domain/user/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "沸点")
+@Tag(name = "用户")
 @RestController
 @RequestMapping("/user")
 public class UserController {
